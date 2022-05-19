ba0ace3d8d4883dab3ebb77237c5efffa7c76909
vijeyash1 <vijeyash@gmail.com>
2022-05-19 23:28:17 +0530

    major change

--- main.go
+++ main.go
@@ -1,192 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
 
+	"github.com/go-git/go-git/v5"
+	. "github.com/go-git/go-git/v5/_examples"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/storage/memory"
+)
+
+// Example of how to:
+// - Clone a repository into memory
+// - Get the HEAD reference
+// - Using the HEAD reference, obtain the commit this reference is pointing to
+// - Using the commit, obtain its history and print it
 func main() {
-	fmt.Println("hi")
-	pushMetrics
-	{
-		"ref": "rfs/heads/ma
-		"before": "c32e6b4a3b9fcab0956825c8f488daff4d0a3ecf",
-		"after": "68e850d4b2ae149ecdc806a0ec5e04edd84ecbff",
-		"repository": {
-			"id": 489336223,
-			"node_id": "R_kgDOHSqtnw",,
-			"name": "webhooktest",
-			"full_name": "vijeyash1/webhooktest",
-			"private": false,
-			"owner": {
-				"name": "vijeyash1",
-				"email": "91282703+vijeyash1@users.noreply.github.com",
-				"login": "vijeyash1",
-				"id": 91282703,
-				"node_id": "MDQ6VXNlcjkxMjgyNzAz",
-				"avatar_url": "https://avatars.githubusercontent.com/u/91282703?v=4",
-				"gravatar_id": "",
-				"url": "https://api.github.com/users/vijeyash1",
-				"html_url": "https://github.com/vijeyash1",
-				"followers_url": "https://api.github.com/users/vijeyash1/followers",
-				"following_url": "https://api.github.com/users/vijeyash1/following{/other_user}",
-				"gists_url": "https://api.github.com/users/vijeyash1/gists{/gist_id}",
-				"starred_url": "https://api.github.com/users/vijeyash1/starred{/owner}{/repo}",
-				"subscriptions_url": "https://api.github.com/users/vijeyash1/subscriptions",
-				"organizations_url": "https://api.github.com/users/vijeyash1/orgs",
-				"repos_url": "https://api.github.com/users/vijeyash1/repos",
-				"events_url": "https://api.github.com/users/vijeyash1/events{/privacy}",
-				"received_events_url": "https://api.github.com/users/vijeyash1/received_events",
-				"type": "User",
-				"site_admin": false
-			},
-			"html_url": "https://github.com/vijeyash1/webhooktest",
-			"description": null,
-			"fork": false,
-			"url": "https://github.com/vijeyash1/webhooktest",
-			"forks_url": "https://api.github.com/repos/vijeyash1/webhooktest/forks",
-			"keys_url": "https://api.github.com/repos/vijeyash1/webhooktest/keys{/key_id}",
-			"collaborators_url": "https://api.github.com/repos/vijeyash1/webhooktest/collaborators{/collaborator}",
-			"teams_url": "https://api.github.com/repos/vijeyash1/webhooktest/teams",
-			"hooks_url": "https://api.github.com/repos/vijeyash1/webhooktest/hooks",
-			"issue_events_url": "https://api.github.com/repos/vijeyash1/webhooktest/issues/events{/number}",
-			"events_url": "https://api.github.com/repos/vijeyash1/webhooktest/events",
-			"assignees_url": "https://api.github.com/repos/vijeyash1/webhooktest/assignees{/user}",
-			"branches_url": "https://api.github.com/repos/vijeyash1/webhooktest/branches{/branch}",
-			"tags_url": "https://api.github.com/repos/vijeyash1/webhooktest/tags",
-			"blobs_url": "https://api.github.com/repos/vijeyash1/webhooktest/git/blobs{/sha}",
-			"git_tags_url": "https://api.github.com/repos/vijeyash1/webhooktest/git/tags{/sha}",
-			"git_refs_url": "https://api.github.com/repos/vijeyash1/webhooktest/git/refs{/sha}",
-			"trees_url": "https://api.github.com/repos/vijeyash1/webhooktest/git/trees{/sha}",
-			"statuses_url": "https://api.github.com/repos/vijeyash1/webhooktest/statuses/{sha}",
-			"languages_url": "https://api.github.com/repos/vijeyash1/webhooktest/languages",
-			"stargazers_url": "https://api.github.com/repos/vijeyash1/webhooktest/stargazers",
-			"contributors_url": "https://api.github.com/repos/vijeyash1/webhooktest/contributors",
-			"subscribers_url": "https://api.github.com/repos/vijeyash1/webhooktest/subscribers",
-			"subscription_url": "https://api.github.com/repos/vijeyash1/webhooktest/subscription",
-			"commits_url": "https://api.github.com/repos/vijeyash1/webhooktest/commits{/sha}",
-			"git_commits_url": "https://api.github.com/repos/vijeyash1/webhooktest/git/commits{/sha}",
-			"comments_url": "https://api.github.com/repos/vijeyash1/webhooktest/comments{/number}",
-			"issue_comment_url": "https://api.github.com/repos/vijeyash1/webhooktest/issues/comments{/number}",
-			"contents_url": "https://api.github.com/repos/vijeyash1/webhooktest/contents/{+path}",
-			"compare_url": "https://api.github.com/repos/vijeyash1/webhooktest/compare/{base}...{head}",
-			"merges_url": "https://api.github.com/repos/vijeyash1/webhooktest/merges",
-			"archive_url": "https://api.github.com/repos/vijeyash1/webhooktest/{archive_format}{/ref}",
-			"downloads_url": "https://api.github.com/repos/vijeyash1/webhooktest/downloads",
-			"issues_url": "https://api.github.com/repos/vijeyash1/webhooktest/issues{/number}",
-			"pulls_url": "https://api.github.com/repos/vijeyash1/webhooktest/pulls{/number}",
-			"milestones_url": "https://api.github.com/repos/vijeyash1/webhooktest/milestones{/number}",
-			"notifications_url": "https://api.github.com/repos/vijeyash1/webhooktest/notifications{?since,all,participating}",
-			"labels_url": "https://api.github.com/repos/vijeyash1/webhooktest/labels{/name}",
-			"releases_url": "https://api.github.com/repos/vijeyash1/webhooktest/releases{/id}",
-			"deployments_url": "https://api.github.com/repos/vijeyash1/webhooktest/deployments",
-			"created_at": 1651838163,
-			"updated_at": "2022-05-06T12:12:08Z",
-			"pushed_at": 1651840333,
-			"git_url": "git://github.com/vijeyash1/webhooktest.git",
-			"ssh_url": "git@github.com:vijeyash1/webhooktest.git",
-			"clone_url": "https://github.com/vijeyash1/webhooktest.git",
-			"svn_url": "https://github.com/vijeyash1/webhooktest",
-			"homepage": null,
-			"size": 0,
-			"stargazers_count": 0,
-			"watchers_count": 0,
-			"language": "Go",
-			"has_issues": true,
-			"has_projects": true,
-			"has_downloads": true,
-			"has_wiki": true,
-			"has_pages": false,
-			"forks_count": 0,
-			"mirror_url": null,
-			"archived": false,
-			"disabled": false,
-			"open_issues_count": 0,
-			"license": null,
-			"allow_forking": true,
-			"is_template": false,
-			"topics": [],
-			"visibility": "public",
-			"forks": 0,
-			"open_issues": 0,
-			"watchers": 0,
-			"default_branch": "main",
-			"stargazers": 0,
-			"master_branch": "main"
-		},
-		"pusher": {
-			"name": "vijeyash1",
-			"email": "91282703+vijeyash1@users.noreply.github.com"
-		},
-		"sender": {
-			"login": "vijeyash1",
-			"id": 91282703,
-			"node_id": "MDQ6VXNlcjkxMjgyNzAz",
-			"avatar_url": "https://avatars.githubusercontent.com/u/91282703?v=4",
-			"gravatar_id": "",
-			"url": "https://api.github.com/users/vijeyash1",
-			"html_url": "https://github.com/vijeyash1",
-			"followers_url": "https://api.github.com/users/vijeyash1/followers",
-			"following_url": "https://api.github.com/users/vijeyash1/following{/other_user}",
-			"gists_url": "https://api.github.com/users/vijeyash1/gists{/gist_id}",
-			"starred_url": "https://api.github.com/users/vijeyash1/starred{/owner}{/repo}",
-			"subscriptions_url": "https://api.github.com/users/vijeyash1/subscriptions",
-			"organizations_url": "https://api.github.com/users/vijeyash1/orgs",
-			"repos_url": "https://api.github.com/users/vijeyash1/repos",
-			"events_url": "https://api.github.com/users/vijeyash1/events{/privacy}",
-			"received_events_url": "https://api.github.com/users/vijeyash1/received_events",
-			"type": "User",
-			"site_admin": false
-		},
-		"created": false,
-		"deleted": false,
-		"forced": false,
-		"base_ref": null,
-		"compare": "https://github.com/vijeyash1/webhooktest/compare/c32e6b4a3b9f...68e850d4b2ae",
-		"commits": [
-			{
-				"id": "68e850d4b2ae149ecdc806a0ec5e04edd84ecbff",
-				"tree_id": "89365475e005b299d15c0703f834ebea710ed96a",
-				"distinct": true,
-				"message": "check",
-				"timestamp": "2022-05-06T18:01:59+05:30",
-				"url": "https://github.com/vijeyash1/webhooktest/commit/68e850d4b2ae149ecdc806a0ec5e04edd84ecbff",
-				"author": {
-					"name": "vijeyash1",
-					"email": "vijeyash@gmail.com",
-					"username": "vijeyash1"
-				},
-				"committer": {
-					"name": "vijeyash1",
-					"email": "vijeyash@gmail.com",
-					"username": "vijeyash1"
-				},
-				"added": [],
-				"removed": [],
-				"modified": [
-					"main.go"
-				]
-			}
-		],
-		"head_commit": {
-			"id": "68e850d4b2ae149ecdc806a0ec5e04edd84ecbff",
-			"tree_id": "89365475e005b299d15c0703f834ebea710ed96a",
-			"distinct": true,
-			"message": "check",
-			"timestamp": "2022-05-06T18:01:59+05:30",
-			"url": "https://github.com/vijeyash1/webhooktest/commit/68e850d4b2ae149ecdc806a0ec5e04edd84ecbff",
-			"author": {
-				"name": "vijeyash1",
-				"email": "vijeyash@gmail.com",
-				"username": "vijeyash1"
-			},
-			"committer": {
-				"name": "vijeyash1",
-				"email": "vijeyash@gmail.com",
-				"username": "vijeyash1"
-			},
-			"added": [],
-			"removed": [],
-			"modified": [
-				"main.go"
-			]
-		}
-	}
+	// Clones the given repository, creating the remote, the local branches
+	// and fetching the objects, everything in memory:
+	Info("git clone https://github.com/vijeyash1/webhooktest")
+	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		URL: "https://github.com/vijeyash1/webhooktest",
+	})
+	CheckIfError(err)
+
+	b, err := r.Branches()
+	CheckIfError(err)
+	var d *plumbing.Reference
+	err = b.ForEach(func(d *plumbing.Reference) error {
+		fmt.Println(d)
+		return nil
+	})
+	CheckIfError(err)
+
+	// Gets the HEAD history from HEAD, just like this command:
+	Info("git log")
+
+	// ... retrieves the branch pointed by HEAD
+	ref, err := r.Head()
+	CheckIfError(err)
+
+	// ... retrieves the commit history
+	since := time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)
+	until := time.Date(2022, 7, 30, 0, 0, 0, 0, time.UTC)
+	cIter, err := r.Log(&git.LogOptions{From: ref.Hash(), Since: &since, Until: &until})
+	CheckIfError(err)
+
+	// ... just iterates over the commits, printing it
+	err = cIter.ForEach(func(c *object.Commit) error {
+		fmt.Println(c)
+
+		return nil
+	})
+	CheckIfError(err)
 }
